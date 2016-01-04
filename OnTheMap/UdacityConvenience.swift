5b00f21a2ef407979dfa90eda6671e332171a7f1
Paul <paul.salina@gmail.com>
2016-01-04 00:52:21 -0500

    change error message handling to use constants and refactor

--- OnTheMap/UdacityConvenience.swift
+++ OnTheMap/UdacityConvenience.swift
@@ -18,29 +18,37 @@ extension UdacityClient {
         ]
         
         taskForPOSTMethod(Methods.SessionID, parameters: [String: AnyObject](), jsonBody: jsonBody) { result, error -> Void in
-            let errorHandler = { (description: String) -> Void in
-                print(description)
-                
-                completionHandler(success: false, errorString: description)
-            }
             
+            // 1) check if there's an error from the POST method
             if let error = error {
-                errorHandler("error: \(error.description)")
+                if error.localizedDescription == "Your request returned an invalid response! Status code: 403!" {
+                    completionHandler(success: false, errorString: ErrorDescription.InvalidEmailPassword)
+                }
+                else if error.localizedDescription.containsString("There was an error with your request") {
+                    completionHandler(success: false, errorString: ErrorDescription.RequestError)
+                }
+                else {
+                    completionHandler(success: false, errorString: ErrorDescription.DataError)
+                }
+                
                 return
             }
             
+            // 2) parse the data for the user id
             guard let account = result[JSONResponseKeys.Account] as? [String: AnyObject] else {
-                errorHandler("error: no account record in result")
+                print("error: no account record in result")
+                completionHandler(success: false, errorString: ErrorDescription.DataError)
                 return
             }
             
             guard let userID = account[JSONResponseKeys.UserID] as? String else {
-                errorHandler("error: no user id in result")
+                print("error: no user id in result")
+                completionHandler(success: false, errorString: ErrorDescription.DataError)
                 return
             }
             
+            // 3) set the user id
             self.userID = userID
-            
             completionHandler(success: true, errorString: nil)
             
             print("found User ID: \(self.userID)")
