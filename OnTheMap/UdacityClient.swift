8d063efab0617bef83b40bf4acf08b1a4ac7932d
Paul <paul.salina@gmail.com>
2016-01-04 14:22:34 -0500

    refactor web service api task to separate class

--- OnTheMap/UdacityClient.swift
+++ OnTheMap/UdacityClient.swift
@@ -12,91 +12,46 @@ class UdacityClient : NSObject {
     
     // MARK: Properties
     
-    /* Shared session */
-    var session: NSURLSession
-    
     /* Authentication state */
     var sessionID : String? = nil
     var userID : String? = nil
     
+    /* helper client */
+    let helperClient : CommonClient
+    
     // MARK: Initializers
     
     override init() {
-        session = NSURLSession.sharedSession()
+        helperClient = CommonClient(url: UdacityClient.Constants.BaseURLSecure, session: NSURLSession.sharedSession())
         super.init()
     }
     
     // MARK: POST
     
     func taskForPOSTMethod(method: String, parameters: [String : AnyObject], jsonBody: [String:AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
-    //func taskForPOSTMethod() -> NSURLSessionDataTask {
         
-        /* 1. Set the parameters */
-        let mutableParameters = parameters
+        let httpHeaders = [
+            ("application/json", "Accept"),
+            ("application/json", "Content-Type"),
+        ]
         
-        /* 2/3. Build the URL and configure the request */
-        let urlString = Constants.BaseURLSecure + method + UdacityClient.escapedParameters(mutableParameters)
-        
-        let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
-        request.HTTPMethod = "POST"
-        
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        do {
-            request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(jsonBody, options: .PrettyPrinted)
-        }
-        
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
+         return helperClient.taskForPOSTMethod(method, parameters: parameters, jsonBody: jsonBody, httpHeaders: httpHeaders) { data, error -> Void in
             
-            /* function to be called when an error occurs from server response */
-            let errorHandler = { (description: String) -> Void in
-                print(description)
-                
-                let userInfo = [NSLocalizedDescriptionKey : description]
-                completionHandler(result: nil, error: NSError(domain: "taskForPOSTMethod", code: 1, userInfo: userInfo))
+            if let error = error {
+                completionHandler(result: nil, error: error)
             }
             
-            
-            /* GUARD: Was there an error? */
-            guard (error == nil) else {
-                errorHandler("There was an error with your request: \(error)")
-                return
-            }
-            
-            /* GUARD: Did we get a successful 2XX response? */
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                let error: String
-                if let response = response as? NSHTTPURLResponse {
-                    error = "Your request returned an invalid response! Status code: \(response.statusCode)!"
-                } else if let response = response {
-                    error = "Your request returned an invalid response! Response: \(response)!"
-                } else {
-                    error = "Your request returned an invalid response!"
-                }
-                
-                errorHandler(error)
-                return
-            }
-            
-            /* GUARD: Was there any data returned? */
             guard let data = data else {
-                errorHandler("No data was returned by the request!")
+                print("there's data returned")
                 return
             }
             
-            /* subset response data! */
             let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
             print(NSString(data: newData, encoding: NSUTF8StringEncoding))
             
-            /* 5/6. Parse the data and use the data (happens in completion handler) */
-            UdacityClient.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
+            CommonClient.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
+            
         }
-        
-        task.resume()
-        
-        return task
     }
     
     class func sharedInstance() -> UdacityClient {
@@ -110,99 +65,22 @@ class UdacityClient : NSObject {
     
     func taskForGETMethod(method: String, parameters: [String : AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
         
-        /* 1. Set the parameters */
-        let mutableParameters = parameters
-        
-        /* 2/3. Build the URL and configure the request */
-        let urlString = Constants.BaseURLSecure + method + UdacityClient.escapedParameters(mutableParameters)
-        let url = NSURL(string: urlString)!
-        let request = NSURLRequest(URL: url)
-        
-        /* 4. Make the request */
-        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+        return helperClient.taskForGETMethod(method, parameters: parameters, httpHeaders: [(String, String)]()) { data, error -> Void in
             
-            /* GUARD: Was there an error? */
-            guard (error == nil) else {
-                print("There was an error with your request: \(error)")
-                return
+            if let error = error {
+                completionHandler(result: nil, error: error)
             }
             
-            /* GUARD: Did we get a successful 2XX response? */
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                if let response = response as? NSHTTPURLResponse {
-                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
-                } else if let response = response {
-                    print("Your request returned an invalid response! Response: \(response)!")
-                } else {
-                    print("Your request returned an invalid response!")
-                }
-                return
-            }
-            
-            /* GUARD: Was there any data returned? */
             guard let data = data else {
-                print("No data was returned by the request!")
+                print("there's data returned")
                 return
             }
             
-            /* subset response data! */
             let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
             print(NSString(data: newData, encoding: NSUTF8StringEncoding))
             
-            /* 5/6. Parse the data and use the data (happens in completion handler) */
-            UdacityClient.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
-        }
-        
-        /* 7. Start the request */
-        task.resume()
-        
-        return task
-    }
-    
-    // MARK: Helpers
-    
-    /* Helper: Substitute the key for the value that is contained within the method name */
-    class func substituteKeyInMethod(method: String, key: String, value: String) -> String? {
-        if method.rangeOfString("{\(key)}") != nil {
-            return method.stringByReplacingOccurrencesOfString("{\(key)}", withString: value)
-        } else {
-            return nil
-        }
-    }
-    
-    /* Helper: Given raw JSON, return a usable Foundation object */
-    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
-        
-        var parsedResult: AnyObject!
-        do {
-            parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
-        } catch {
-            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
-            completionHandler(result: nil, error: NSError(domain: "parseJSONWithCompletionHandler", code: 1, userInfo: userInfo))
-        }
-        
-        completionHandler(result: parsedResult, error: nil)
-    }
-    
-    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
-    class func escapedParameters(parameters: [String : AnyObject]) -> String {
-        
-        var urlVars = [String]()
-        
-        for (key, value) in parameters {
-            
-            /* Make sure that it is a string value */
-            let stringValue = "\(value)"
-            
-            /* Escape it */
-            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
-            
-            /* Append it */
-            urlVars += [key + "=" + "\(escapedValue!)"]
+            CommonClient.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
             
         }
-        
-        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
     }
-    
 }
