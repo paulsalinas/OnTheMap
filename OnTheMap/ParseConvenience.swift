a11b612f0e5bac2c4c79bc20e1ce496cef92bd9b
Paul <paul.salina@gmail.com>
2016-01-04 17:23:08 -0500

    parse results from get locations and pass it to the completionHandler

--- OnTheMap/ParseConvenience.swift
+++ OnTheMap/ParseConvenience.swift
@@ -25,7 +25,28 @@ extension ParseClient {
                 return
             }
             
-            print(result)
+            guard let result = result[JSONResponseKeys.Results] as? [[String: AnyObject]] else {
+                print("couldn't find result key")
+                return
+            }
+            
+            completionHandler(users: ParseClient.udaciansFromResults(result), errorString: nil)
         }
+    }
+    
+    class func udaciansFromResults(results: [[String: AnyObject]]) -> [Udacian] {
+        var udacians = [Udacian]()
+        
+        for result in results {
+            let udacian = Udacian(
+                firstName: result[JSONResponseKeys.FirstName] as! String,
+                lastName: result[JSONResponseKeys.LastName] as! String,
+                userId: result[JSONResponseKeys.UserID] as! String
+            )
+            
+            udacians.append(udacian)
+        }
+        
+        return udacians
     }
 }
