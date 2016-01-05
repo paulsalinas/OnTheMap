4e02e0a0590f1cd89166114266b9f4197ff8b31a
Paul <paul.salina@gmail.com>
2016-01-04 22:50:53 -0500

    remove test code and unneeded properties

--- OnTheMap/OnTheMapTabBarController.swift
+++ OnTheMap/OnTheMapTabBarController.swift
@@ -9,20 +9,8 @@
 import UIKit
 
 class OnTheMapTabBarController: UITabBarController {
-
-    var user: StudentInformation!
-    var parseClient: ParseClient!
-    var userLocations: [StudentInformation]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        //initialize parseClient and get the user locations
-        parseClient = ParseClient.sharedInstance()
-        parseClient.getUserLocations() { users, errorString -> Void in
-            print(users)
-            return
-        }
     }
 }
